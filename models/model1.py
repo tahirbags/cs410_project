a6158d0051eb2835eed59093965325fb8d216b4c
hkv2 <73406159+hkv2@users.noreply.github.com>
2023-11-12 19:55:35 -0600

    model1.py

---
+++ models/model1.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LogisticRegression
+
+class LogisticModel:
+    def __init__(self):
+        self.model = LogisticRegression()
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
