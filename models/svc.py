6ca59a6ff65f78bca335f9679d5a7d073a21f82f
tahirbags <144145146+tahirbags@users.noreply.github.com>
2023-11-25 10:04:08 -0500

    Update svc.py

--- models/svc.py
+++ models/svc.py
@@ -1,10 +1,14 @@
-from sklearn.svm import svm
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
 class SVCModel: #Support Vector Classification
     def __init__(self):
-        self.model = svm.SVC (gamma='auto')
+        self.model = LinearSVC(C=1.0, class_weight=None, dual=True, fit_intercept=True,
+          intercept_scaling=1, loss='squared_hinge', max_iter=1000,
+          multi_class='ovr', penalty='l2', random_state=None, tol=0.0001,
+          verbose=0)
+        #self.model = LinearSVC(verbose=0)
 
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
