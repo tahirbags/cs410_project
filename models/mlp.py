a3c95f5cab89bcee0d5e8bb9e76ce3c8900003da
tahirbags <144145146+tahirbags@users.noreply.github.com>
2023-12-02 17:11:05 -0600

    Update mlp.py
    Code comments on hyperparameter optimization.
    MLP Classifier updated with best hyperparameters.

--- models/mlp.py
+++ models/mlp.py
@@ -1,8 +1,26 @@
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+
+'''
+Hyperparameter selection:
+
+1. Run mlp_hpo.py
+
+2. Using GridSearchCV to find the optimal hyperparameters
+
+3. Set parameter_space to list hyperparameters to tune
+
+4. n_jobs -1 is for using all the CPU cores available, cv=5 is for cross validation, here it means 5-folds Stratified K-fold cross validation
+
+5. Print best parameters
+
+6. Update best parameters in mlp.py
+
+'''
 
 class MLPClassifierModel:
     def __init__(self):
-        self.model = MLPClassifier(alpha=1, max_iter=1000, random_state=42)
+        self.model = MLPClassifier(activation ='tanh', alpha=0.05, learning_rate='adaptive', solver='sgd')
 
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
