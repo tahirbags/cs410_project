be98e4954c361cbc0cbc473105d669820a87b00c
asagron <46514929+asagron@users.noreply.github.com>
2023-12-04 17:54:17 -0700

    Update svc.py

--- models/svc.py
+++ models/svc.py
@@ -2,14 +2,32 @@ from sklearn.svm import LinearSVC
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
-class SVCModel: #Support Vector Classification
+class SVCModel:
+    """
+   A class used to represent a Support Vector Classification Model
+
+   Attributes
+   ----------
+   model :
+       an instance of the LinearSVC Class from sklearn
+
+   Methods
+   -------
+   train(X_train, y_train)
+       Trains the model using given training data
+   predict(X_test)
+       Predicts labels for given test data
+   """
+
     def __init__(self):
+        """
+        Hyperparameter tuning:
+        """
         self.model = LinearSVC(C=1.0, class_weight=None, dual=True, fit_intercept=True,
           intercept_scaling=1, loss='squared_hinge', max_iter=1000,
           multi_class='ovr', penalty='l2', random_state=None, tol=0.0001,
           verbose=0)
-        #self.model = LinearSVC(verbose=0)
-
+        
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
 
