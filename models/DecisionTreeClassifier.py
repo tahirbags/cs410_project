afa084e6859ac89d206a0570eba9100d4332e5e8
tahirbags <144145146+tahirbags@users.noreply.github.com>
2023-12-10 17:24:53 -0600

    Update DecisionTreeClassifier.py

--- models/DecisionTreeClassifier.py
+++ models/DecisionTreeClassifier.py
@@ -21,7 +21,9 @@ class DecisionTreeClassifierModel:
         """
          Hyperparameter selection:
          List of parameters that can be tuned:
-         dict_keys(['ccp_alpha', 'class_weight', 'criterion', 'max_depth', 'max_features', 'max_leaf_nodes', 'min_impurity_decrease', 'min_impurity_split', 'min_samples_leaf', 'min_samples_split', 'min_weight_fraction_leaf', 'random_state', 'splitter'])
+         dict_keys(['ccp_alpha', 'class_weight', 'criterion', 'max_depth', 'max_features', 'max_leaf_nodes', 
+         'min_impurity_decrease', 'min_impurity_split', 'min_samples_leaf', 'min_samples_split', 'min_weight_fraction_leaf', 
+         'random_state', 'splitter'])
         """
         self.model = DecisionTreeClassifier(max_depth=5, random_state=42)
 
