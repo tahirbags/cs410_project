6df18b99aee4238b094cae73ca4f764bb407df24
tahirbags <144145146+tahirbags@users.noreply.github.com>
2023-12-13 00:35:08 -0600

    Update AdaBoostClassifier.py

--- models/AdaBoostClassifier.py
+++ models/AdaBoostClassifier.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 
 class AdaBoostClassifierModel:
     def __init__(self):
-        self.model = AdaBoostClassifier(n_estimators=500, max_features="sqrt", min_samples_split=20, criterion="gini", random_state=42), learning_rate=0.1)
+        self.model = AdaBoostClassifier(n_estimators=500, max_features="sqrt", min_samples_split=20, criterion="gini", random_state=42), learning_rate=0.1, n_estimators=50)
 
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
